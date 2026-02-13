0791a6028962ada943eef995d8adc67a8f5dbd4d
huzaifa-codes <huzaifabusiness60@gmail.com>
2026-02-13 15:30:59 +0500

    changes updated

--- package/package/src/pages/Home2.tsx
+++ package/package/src/pages/Home2.tsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { useEffect } from "react";
 import MainBanner2 from "../components/MainBanner2";
-import { Home2IconWrapper } from "../elements/JsonData";
+
 import Home2OurMenu from "../components/Home2OurMenu";
 import Home2SpacialMenu from "../components/Home2SpacialMenu";
 import { IMAGES } from "../constent/theme";
-import Home2AreMenu from "../components/Home2AreMenu";
-import TeamCarosel from "../components/TeamCarosel";
+import Home3OurMenu from "../components/Home3OurMenu";
+
 import Home2Testimonial from "../components/Home2Testimonial";
-import Home2OurBlog from "../components/Home2OurBlog";
-import { useEffect } from "react";
+
+
 
 const Home2 = () => {
   useEffect(() => {
@@ -17,30 +17,8 @@ const Home2 = () => {
   return (
     <div className="page-content bg-white">
       <MainBanner2 />
-      <section className="content-inner bg-white">
-        <div className="container">
-          <div className="row icon-wrapper2 gx-lg-5">
-            {Home2IconWrapper.map(({ icon, title }, ind) => (
-              <div className="col-lg-4 col-md-6 m-b30 wow fadeInUp" key={ind}>
-                <div className="icon-bx-wraper style-2">
-                  <div className="icon-bx radius">
-                    <span className="icon-cell">
-                      <i className={icon}></i>
-                    </span>
-                  </div>
-                  <div className="icon-content">
-                    <h5 className="dz-title">
-                      <Link to="/services">{title}</Link>
-                    </h5>
-                    <p>lorem ipsum dolor shit amet lorem ipsum</p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-      <section className="content-inner-1 overflow-hidden pt-0">
+
+      <section className="content-inner-1 overflow-hidden mt-5">
         <div className="container">
           <div className="section-head menu-align">
             <h2 className="title mb-0 wow flipInX">Browse Our Menu</h2>
@@ -74,23 +52,13 @@ const Home2 = () => {
       <section className="content-inner-1">
         <div className="container">
           <div className="section-head text-center">
-            <h2 className="title wow flipInX">Are Menu</h2>
+            <h2 className="title wow flipInX">Our Menu</h2>
           </div>
-          <Home2AreMenu />
+          <Home3OurMenu />
         </div>
       </section>
-      <section
-        className="section-wrapper-4 content-inner overflow-hidden bg-parallax"
-        style={{ backgroundImage: `url(${IMAGES.background_pic10})` }}
-      >
-        <div className="container">
-          <div className="section-head text-center">
-            <h2 className="title wow flipInX">Meet The</h2>
-          </div>
-          <TeamCarosel />
-        </div>
-      </section>
-      <section className="content-inner-2 overflow-hidden">
+
+      <section className="content-inner-2 overflow-hidden" style={{ marginBottom: '100px' }}>
         <div className="container">
           <div className="section-head text-center">
             <h2 className="title wow flipInX">Customer's Comment</h2>
@@ -98,7 +66,7 @@ const Home2 = () => {
           <Home2Testimonial />
         </div>
       </section>
-      <Home2OurBlog />
+
     </div>
   );
 };
