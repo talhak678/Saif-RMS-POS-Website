6fc4d37e3d13762bdd4da8029227a7637ef1f075
huzaifa-codes <huzaifabusiness60@gmail.com>
2026-02-27 21:54:34 +0500

    chages

--- package/package/src/elements/CommonBanner.tsx
+++ package/package/src/elements/CommonBanner.tsx
@@ -1,34 +1,33 @@
-import { Link } from "react-router-dom";
-
 interface propFile {
   img: string;
   title: string;
-  subtitle: string;
+  subtitle?: string;
 }
 
-const CommonBanner = ({ img, title, subtitle }: propFile) => {
+const CommonBanner = ({ img, title }: propFile) => {
   return (
     <div
-      className="dz-bnr-inr style-1 text-center bg-parallax bg-parallax"
+      className="dz-bnr-inr style-1 text-center bg-parallax"
       style={{
         backgroundImage: `url(${img})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
+      <style>
+        {`
+          .dz-bnr-inr.style-1::after {
+            background: rgba(0, 0, 0, 0.3) !important;
+          }
+          .dz-bnr-inr.style-1 .dz-bnr-inr-entry h1 {
+            margin-bottom: 0;
+            text-shadow: 2px 2px 10px rgba(0,0,0,0.5);
+          }
+        `}
+      </style>
       <div className="container">
         <div className="dz-bnr-inr-entry">
           <h1>{title}</h1>
-          <nav aria-label="breadcrumb" className="breadcrumb-row">
-            <ul className="breadcrumb">
-              <li className="breadcrumb-item">
-                <Link to="/">Home</Link>
-              </li>
-              <li className="breadcrumb-item active" aria-current="page">
-                {subtitle}
-              </li>
-            </ul>
-          </nav>
         </div>
       </div>
     </div>
